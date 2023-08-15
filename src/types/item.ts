59ced56b4d972e21456de5cbb5c9eb70a542a37b
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-15 15:50:41 +0330

    define interfaces for a couple of entities

---
+++ src/types/item.ts
@@ -0,0 +1,47 @@
+export interface Category {
+  id: number;
+  image: string;
+  title: string;
+  slug: string;
+  created_at: string | null;
+  updated_at: string | null;
+}
+export interface Seo {
+  id: number;
+  image: string;
+  title: string;
+  slug: string;
+  created_at: string | null;
+  updated_at: string | null;
+}
+export interface Images {
+  small: Image;
+  large: Image;
+}
+export interface Image {
+  url: string;
+  width: number;
+  height: number;
+}
+export interface User {
+  id: numner;
+  avatar: string;
+  bio: string;
+  username: string;
+  collection_count: number;
+  name: string;
+  business_data: string;
+  created_at: string;
+}
+export interface Item {
+  id: number;
+  title: string;
+  name: string;
+  description: string;
+  color: string;
+  type: string;
+  seo: Seo;
+  category: Category;
+  images: Images;
+  user: User;
+}
