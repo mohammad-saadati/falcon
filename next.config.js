da2803257448b79187cca53271b3155e84e2e67a
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-16 11:19:47 +0330

    add remote images source

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.didanist.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.didanist.com/",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
