efa077c556eff4955dc6d4ac68250b186e480889
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-16 11:20:28 +0330

    define a children slot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="container mx-auto">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
