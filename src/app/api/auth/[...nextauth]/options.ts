6a7d405c57b1c5c706e8e40e593a3e61628d8885
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-27 15:00:25 +0330

    write first nextjs provider

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,45 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "username",
+          placeholder: "username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        try {
+          const response = await fetch(
+            "https://didanist.com/api/v1_0/user/login",
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(credentials),
+            }
+          );
+
+          const data = await response.json();
+
+          if (response.ok && data.success) {
+            return {};
+          } else {
+            return { msb: "error happened" };
+          }
+        } catch (err) {
+          return { err };
+        }
+      },
+    }),
+  ],
+  pages: {},
+};
