4f16e23eb57d8823b042f176a42783618cda22f1
mohammad_saadati <saadati.m.b@gmail.com>
2023-09-11 22:22:45 +0330

    edit page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,10 +1,10 @@
 import Login from "@/components/Forms/Login";
 import CheckExist from "@/components/Forms/CheckExist";
 
-export default function Signin() {
+export default function SignIn() {
   return (
     <>
-      <CheckExist />
+      <Login />
     </>
   );
 }
