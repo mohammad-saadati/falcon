6f2e40ce0749c19dfd0f509dfcd48075b49386d8
mohammad_saadati <saadati.m.b@gmail.com>
2023-09-17 11:53:23 +0330

    fix base url

--- src/utilities/axios.ts
+++ src/utilities/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://didanist.com/api",
+  baseURL: "https://didanist.com/api/v1_0/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
